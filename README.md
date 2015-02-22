69c072afc2b445f7cfd82e236fbabb7a75f21735
Jef Maerien <jef.maerien@gmail.com>
2015-02-22 16:28:21 +0100

    update to fill in assignment

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-first submit
+Data cleaning programming assignment
+=================================
+
+## Assumptions
+
+The script assumes that it is executed inside the  "UCI HAR DATASET" folder, and will create a new file inside that folder with the output
+
+I also assume that we only want those variables that have a mean and std present in the data set. These variables are listed in the code book file. 
+I realize that in the data there are some meanFreq variables, but i chose to ignore them since I don't consider them means. They are easily added by removing the FIXED=TRUE command
+
+## Operation
+
+The script runs a bit in a different order than strictly required, because i want to reduce the size of the data sets as soon as possible.
+
+As such, the script first reads the features, and parses all mean and std features from the features list. This gives the names and column index of the required features.
+
+Once the required features are established, the script reads in the data as follows:
+*  read in X data set of train and test
+* remove unwanted data
+* merge X data
+* add names
+* read in the action data of train and test
+* merge action data
+* add names
+* read in subject data of train and test
+* merge subject data
+* add names
+* merge subject, Y, and X data in single data frame.
+
+Note that during merging, the training set always comes first, the test set second.
+
+Once all data is loaded, the script runs a loop over all subjects and actions. For each subject - action pair it takes the all the data, calculates the mean, and stores it in a new data set.
+
+Next, once the final data is gathered, the script replaces all numeric indices of actions with human readable actions. I do this last, since i think the script operates faster when handling numeric data as opposed to character data.
+Finally, the data is writted to a txt file. I did not write it to a csv file, because the file upload button in coursera only supports limited extensions, of which txt is one, and the data format is space delimited, instead of comma separated value.
+
+
+
+
+ 
---
+++ run_analysis.R
@@ -0,0 +1,79 @@
+####
+## Decide which variables should be gathered, by looking at the features list
+####
+
+## read in features
+features = read.table("features.txt")
+
+## only get interesting features => mean and standard deviation
+## note that there are also freq mean variables, but i did not think they should have been added. 
+## 
+meanIndex = grep("mean()",features[,2],fixed=TRUE)
+stdIndex = grep("std",features[,2],fixed=TRUE)
+## create index of mean and the names
+totalIndex = c(meanIndex,stdIndex)
+## order totalIndex
+totalIndex = totalIndex[order(totalIndex)]
+## get variable names, remove default factor variable
+totalNames = as.character(features[totalIndex,2])
+
+####
+## Read in data, merge data sets, merge with subject and action
+####
+
+## read in test data
+testData = read.table("test//X_test.txt")
+testData = testData[,totalIndex]
+## read in train data
+trainData = read.table("train//X_train.txt")
+trainData = trainData[,totalIndex]
+#merge data
+totalData = rbind(trainData,testData)
+names(totalData) = totalNames
+## clear large unneded vars
+remove(testData,trainData)
+
+testAction = read.table("test//y_test.txt")
+trainAction = read.table("train/y_train.txt")
+
+totalAction = rbind(trainAction,testAction)
+
+
+testSubject = read.table("test//subject_test.txt")
+trainSubject = read.table("train//subject_train.txt")
+
+totalSubject = rbind(trainSubject,testSubject)
+
+remove(testSubject,trainSubject,testAction,trainAction)
+
+names(totalAction) = c("action")
+names(totalSubject) = c("subject")
+
+totalData = cbind(totalSubject,totalAction,totalData)
+
+
+####
+## For each subject and action that we have, go through data and calculate mean.
+## Bind into new data frame
+## This can likely be done in a single exotic R command, but this to me is more clear
+####
+
+newData = data.frame()
+for( subject in 1:max(totalSubject)){
+    for( action in 1:max(totalAction)){
+        val = totalData[totalData$subject==subject & totalData$action==action,]
+        
+        newData = rbind(newData,sapply(val,mean))
+    }
+}
+names(newData) = names(totalData)
+
+## add human readable activity labels
+labels = read.table("activity_labels.txt")
+
+for(index in 1:nrow(newData)){
+    newData$action[index] = as.character(labels[labels$V1==newData$action[index],2])
+}
+
+## write to table
+write.table(newData,"dataCleaningAssignment.txt")
